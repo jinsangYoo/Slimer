b59227c009cbdbcfebe2e469623313cf3cbc919c
jinsang <jinsang@nhndata.com>
2021-05-10 18:33:59 +0900

    더미 request 작업중 - axios 추가

--- src/ts/ACS.ts
+++ src/ts/ACS.ts
@@ -1,4 +1,6 @@
 import { ACParams } from "./ACParams";
+import axios from "axios";
+import { AxiosRequestConfig } from "axios";
 
 export class ACS {
   private static instance: ACS;
@@ -12,5 +14,72 @@ export class ACS {
   send(value: ACParams): void {
     // return ACS.getInstance().;
     console.log("ACS.send: " + JSON.stringify(value));
+    axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+    let config: AxiosRequestConfig = {
+      url: "policy",
+      method: "get",
+      baseURL: "https://policy.acecounter.com",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
+        "Content-Type": "application/json",
+        crossdomain: true,
+
+        "CP-Request-Version": "00.00.01",
+        "CP-Request-Cid": "1543932249276699286",
+        "CP-Request-Time": "1543933205978",
+        "CP-Request-Platform": "ts",
+        "CP-Request-ID": "AK2A79936"
+      },
+      timeout: 1000
+    };
+
+    let localConfig: AxiosRequestConfig = {
+      url: "policy",
+      method: "get",
+      baseURL: "http://127.0.0.1:52274",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
+        "Content-Type": "application/json",
+
+        "CP-Request-Version": "00.00.01",
+        "CP-Request-Cid": "1543932249276699286",
+        "CP-Request-Time": "1543933205978",
+        "CP-Request-Platform": "ts",
+        "CP-Request-ID": "AK2A79936"
+      },
+      timeout: 1000
+    };
+    // axios
+    //   .create()
+    //   // .request(localConfig)
+    //   .request(config)
+    //   .then(response => {
+    //     console.log("success");
+    //     console.log(response);
+    //     console.log(response.data);
+    //   })
+    //   .catch(error => {
+    //     console.log("error!!");
+    //     console.log(error);
+    //   });
+    axios
+      .create()
+      // .get("policy", localConfig)
+      .get("policy", config)
+      .then(response => {
+        console.log("success");
+        console.log(response);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log("error!!");
+        console.log(error);
+      });
   }
 }
